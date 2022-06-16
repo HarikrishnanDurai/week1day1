09cc754b4203b068f8ff34b56f1bc04e450fc00e
HarikrishnanDurai <107620245+HarikrishnanDurai@users.noreply.github.com>
2022-06-16 20:44:06 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,18 @@
+package Week1;
+
+public class FibonacciSeries {
+	
+		public static void main(String[] args) {
+			int n = 8, firstNum = 0, secNum = 1;
+			System.out.println("Fibonacci Series " + n + " terms:");
+			for (int i = 1; i < n; ++i) {
+				int sum = firstNum + secNum;
+				firstNum = secNum;
+				secNum = sum;
+				System.out.println(sum);
+				
+			}
+		}
+
+	}
+
